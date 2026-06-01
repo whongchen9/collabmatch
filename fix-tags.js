b76dbfaaeac4789db471ba2d7b50a47188eb7ef9
CollabMatch <dev@collabmatch.app>
2026-06-01 22:37:28 +0800

    chore: clean up temp scripts

--- fix-tags.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let s = fs.readFileSync('index.html', 'utf8');
-s = s.replace(/<motion /g, '<div ').replace(/<\/motion>/g, '\u003c/div\u003e');
-fs.writeFileSync('index.html', s);
-console.log('ok');
